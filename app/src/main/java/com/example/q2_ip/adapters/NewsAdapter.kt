d4fc04a6a83fdf331bb42b31166cf86681ef929d
Hrithik-Joshi <84170164+Hrithik-Joshi@users.noreply.github.com>
2023-06-27 14:49:25 +0530

    fix crashes

--- app/src/main/java/com/example/q2_ip/adapters/NewsAdapter.kt
+++ app/src/main/java/com/example/q2_ip/adapters/NewsAdapter.kt
@@ -45,7 +45,7 @@ class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ArticleViewHolder>() {
         val binding = holder.binding
         holder.itemView.apply {
             Glide.with(this).load(article.urlToImage).into(binding.ivArticleImage)
-            binding.tvSource.text = article.source.name
+            binding.tvSource.text = article.source?.name
             binding.tvTitle.text = article.title
             binding.tvDescription.text = article.description
             binding.tvPublishedAt.text = article.publishedAt
